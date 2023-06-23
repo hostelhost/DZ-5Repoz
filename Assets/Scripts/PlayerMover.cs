587a584135a525ba9ac4c29616e4259f4cbebc53
Илья <Hostel_2008@mail.ru>
2023-06-23 17:59:01 +0300

    Исправление

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private float _speed;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _speed = 3.0f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D)) 
+        { 
+            _player.transform.Translate(1 * _speed * Time.deltaTime, 0, 0);
+            _spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _player.transform.Translate(-1 * _speed * Time.deltaTime, 0, 0);
+            _spriteRenderer.flipX = true;
+        }
+    }
+}
